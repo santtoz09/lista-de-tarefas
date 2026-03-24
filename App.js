5140d5b2a62925513e603da32de992b3c811f953
santtoz09 <santozeliasp@outlook.com>
2026-03-24 17:56:05 -0300

    fim da aula do react - commit

--- App.js
+++ App.js
@@ -1,18 +1,49 @@
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const tarefas = [
+    {id : "1",titulo :"Aprender Git"},
+    {id : "2",titulo :"Aprender fazer Comit" },
+    {id : "3",titulo :"Aprender utilizar o GitHub"},
+  ] 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Lista de tarefas</Text>
+
+      <FlatList 
+      data={tarefas}
+      keyExtractor={(item) =>item.id}
+      renderItem={({item}) =>(
+
+
+        <Text style={{color:"white"}}>{item.titulo}</Text>
+      )}
+
+
+      />
     </View>
   );
 }
 
+
+
+
+
+
+// css
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0f172a',
+
   },
+  text: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 20,
+
+  }
 });
