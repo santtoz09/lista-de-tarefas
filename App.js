5666702a33a5df6dc0c6c173593a360c62fc090d
santtoz09 <santtozeliasp@outlook.com>
2026-03-31 17:26:24 -0300

    second commit

--- App.js
+++ App.js
@@ -3,23 +3,25 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 export default function App() {
 
   const tarefas = [
-    {id : "1",titulo :"Aprender Git"},
-    {id : "2",titulo :"Aprender fazer Comit" },
-    {id : "3",titulo :"Aprender utilizar o GitHub"},
-  ] 
+    { id: "1", titulo: "Aprender Git" },
+    { id: "2", titulo: "Aprender fazer Comit" },
+    { id: "3", titulo: "Aprender utilizar o GitHub" },
+    { id: "4", titulo: "fazer git clone" },
+
+  ]
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Lista de tarefas</Text>
 
-      <FlatList 
-      data={tarefas}
-      keyExtractor={(item) =>item.id}
-      renderItem={({item}) =>(
+      <FlatList
+        data={tarefas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
 
 
-        <Text style={{color:"white"}}>{item.titulo}</Text>
-      )}
+          <Text style={styles.item}>{item.titulo}</Text>
+        )}
 
 
       />
@@ -37,13 +39,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#0f172a',
-
   },
+
+
   text: {
     fontSize: 48,
     fontWeight: 'bold',
     color: 'white',
-    marginBottom: 20,
+    margin: 20,
+  },
 
-  }
+
+  item: {
+    color: "white",
+    fontSize: 30,
+    borderRadius: 12,
+    backgroundColor: '#3b5ba449',
+    margin: 20,
+    padding: 10,
+  },
+
+
 });
